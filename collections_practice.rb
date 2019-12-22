3d237165f9074c1164d1cba4836781e8afa09277
Mikael Kaya <kayaswork@gmail.com>
2019-12-22 09:27:33 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -7,3 +7,16 @@ swap_elements_from_to(["a", "b", "c"],0,2)
 swap_elements_from_to(["a", "b", "c"],2,1)
 
 end
+
+
+def assort(arr)
+  i = 0
+  j = 1
+  x = []
+  until (arr[i] > arr[j]) do
+     x.insert(i, arr[j], arr[i] )
+     i += 1
+     j += 1
+  end
+print x
+end code.
