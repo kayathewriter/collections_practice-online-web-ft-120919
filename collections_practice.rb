f9b500c5343632b15e2c6238f20d1714e0503adb
Mikael Kaya <kayaswork@gmail.com>
2019-12-22 09:18:21 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -3,3 +3,12 @@ describe 'swap_elements_from_to'do
     expect(swap_elements_from_to([1,2,10], 2, 0)).to eq([10,2,1])
 end
 end
+
+def swap_elements_from_to(arr,i,to)
+  sav = arr[i]
+  savd = arr[to]
+  arr.delete_at(i)
+  arr[to] = sav
+  arr[i] = savd
+  arr
+end
