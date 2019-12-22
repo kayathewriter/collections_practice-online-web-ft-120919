1c552585004b4a8d57f653391bedf22d5e15aade
Mikael Kaya <kayaswork@gmail.com>
2019-12-22 09:17:58 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,5 @@
-
+describe 'swap_elements_from_to'do
+  it 'should return an array sorted by the index from location into the argument to index location.' do
+    expect(swap_elements_from_to([1,2,10], 2, 0)).to eq([10,2,1])
+end
+end
